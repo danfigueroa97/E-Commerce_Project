91414ddf0ccc8515bfc361ffb266fc737e91be27
JhonAlmanzarQ <jhonalmanzar003@gmail.com>
2024-12-06 16:13:44 -0500

    Add files via upload

---
+++ E-Commerce_Project/src/main/java/com/entorno/E_Commerce_Project/ECommerceProjectApplication.java
@@ -0,0 +1,13 @@
+package com.entorno.E_Commerce_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceProjectApplication.class, args);
+	}
+
+}
---
+++ E-Commerce_Project/src/test/java/com/entorno/E_Commerce_Project/ECommerceProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.entorno.E_Commerce_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
