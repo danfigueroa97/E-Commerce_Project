985e773c1b715af6b59678be56c9c57607c88fbf
skipword <orduzcarvajal.cristian2017@gmail.com>
2024-12-10 10:35:54 -0500

    Update QRRepository.java

--- E-Commerce_Project/src/main/java/com/entorno/E_Commerce_Project/repository/QRRepository.java
+++ E-Commerce_Project/src/main/java/com/entorno/E_Commerce_Project/repository/QRRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface QRRepository extends MongoRepository<QR, Integer> {
+public interface QRRepository extends MongoRepository<QR, String> {
 }
